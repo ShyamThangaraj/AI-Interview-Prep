9269848fce69682ac800db4ac98b31088392a5b5
Arya Palanivel <aryapalanivel@gmail.com>
2025-08-04 18:54:57 -0700

    minor change to ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
         className="absolute left-[71%] top-[40%] w-[30rem] h-[13rem] bg-contain bg-no-repeat bg-center text-sm leading-tight break-words text-black font-mono px-[9rem] pt-[5.3rem] text-center hidden sm:block"
         style={{ backgroundImage: 'url("/cloud.png")' }}
       >
-        “It even helped me with behavioral questions!”<br />— Rina
+        “It even helped me with behavioral questions”<br />— Rina
       </div>
 
       <div
